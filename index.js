1c50cd487a99f0909282ae8f9206e04292dfb1ab
elisey00 <eliseyoung000@gmail.com>
2018-08-08 20:21:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,8 +16,7 @@ function destructivelyRemoveFirstDriver(){
   drivers.shift();
 }
 function appendDriver(name){
-  
-  var newDrivers = drivers;
+  var newDrivers = drivers.slice(0);
   newDrivers.push(name);
   return newDrivers; 
 }
